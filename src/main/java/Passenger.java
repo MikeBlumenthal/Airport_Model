e1a83149543918f2d6d81e9b5c75a36ed237870d
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-12 11:39:29 +0100

    initialise done for passenger

---
+++ src/main/java/Passenger.java
@@ -0,0 +1,9 @@
+public class Passenger {
+    private String name;
+    private int wallet;
+
+    public Passenger(String name, int wallet){
+        this.name = name;
+        this.wallet = wallet;
+    }
+}
