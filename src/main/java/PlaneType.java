b0265786270ad87827e77279be8e3d0fd4b30a4c
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-12 14:44:09 +0100

    adds PLaneType enum with capacity field

--- src/main/java/PlaneType.java
+++ src/main/java/PlaneType.java
@@ -1,2 +1,17 @@
 public enum PlaneType {
+    BOEING747(400),
+    BOEING767(200),
+    BOEING737(100),
+    DOUGLASDC9(80),
+    LEARJET40(10);
+
+    private final int capacity;
+
+    PlaneType(int capacity){
+        this.capacity = capacity;
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
 }
